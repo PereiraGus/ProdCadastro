952fafba298cf98d9e721b5cb9c2d5c778ae0a50
PereiraGus <gustavo.oito@hotmail.com>
2021-06-11 10:27:13 -0300

    Mudanças na validação dos atributos.

--- Models/Prod.cs
+++ Models/Prod.cs
@@ -9,24 +9,23 @@ namespace ProdCadastro.Models
 {
     public class Prod
     {
-        [Range(1, 1000, ErrorMessage = "O código do produto deve ser entre 1 e 1000.")]
-        [Remote("UniqueCod", "HomeController", ErrorMessage = "Esse código já existe.")]
+        [Range(1, 100, ErrorMessage = "O código do produto deve ser entre 1 e 100.")]
         public int Codigo { get; set; }
 
         [Required(ErrorMessage = "O campo nome é obrigatório.")]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "O campo descrição é obrigatório.")]
-        [StringLength(200, MinimumLength = 10, ErrorMessage = "A descrição deve ser entre 10 e 200 caracteres.")]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "A descrição deve ter no mínimo 10 caracteres.")]
         public string Descricao { get; set; }
 
-        [Range(1.00, 1000.00, ErrorMessage = "O peso deve ser entre 1kg e 1000kg")]
-        public decimal Peso { get; set; }
+        [Range(1, 100, ErrorMessage = "O peso deve ser entre 1kg e 100kg")]
+        public double Peso { get; set; }
 
         [Required(ErrorMessage = "O campo categoria é obrigatório.")]
         public string Categoria { get; set; }
 
-        [Range(0.01, 10000, ErrorMessage = "O preço deve ser no mínimo de um centavo e no máximo dez mil reais.")]
-        public decimal PrecoUnitario { get; set; }
+        [Range(0.01, 9999, ErrorMessage = "O preço deve ser no mínimo de um centavo.")]
+        public double PrecoUnitario { get; set; }
     }
 }
