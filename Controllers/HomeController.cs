90fdeafc2a1f3bc664c2fa0337d1ded64ee5c4da
PereiraGus <gustavo.oito@hotmail.com>
2021-06-11 10:01:36 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Collections.ObjectModel;
+using ProdCadastro.Models;
+
+namespace ProdCadastro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var prod = new Prod();
+            return View(prod);
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(Prod prod)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("Result", prod);
+            }
+            return View(prod);
+        }
+
+        public ActionResult Result(Prod prod)
+        {
+            return View(prod);
+        }
+
+        public ActionResult UniqueCod(int Codigo)
+        {
+            var dbExample = new Collection<int>
+            {
+                1,
+                2,
+                3
+            };
+            return Json(JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/Prod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProdCadastro.Models
+{
+    public class Prod
+    {
+        [Range(1, 1000, ErrorMessage = "O código do produto deve ser entre 1 e 1000.")]
+        [Remote("UniqueCod", "HomeController", ErrorMessage = "Esse código já existe.")]
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo descrição é obrigatório.")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "A descrição deve ser entre 10 e 200 caracteres.")]
+        public string Descricao { get; set; }
+
+        [Range(1.00, 1000.00, ErrorMessage = "O peso deve ser entre 1kg e 1000kg")]
+        public decimal Peso { get; set; }
+
+        [Required(ErrorMessage = "O campo categoria é obrigatório.")]
+        public string Categoria { get; set; }
+
+        [Range(0.01, 10000, ErrorMessage = "O preço deve ser no mínimo de um centavo e no máximo dez mil reais.")]
+        public decimal PrecoUnitario { get; set; }
+    }
+}
